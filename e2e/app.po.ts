36614ab7fa2c71227d0ddb43ba08aac42370caa3
venkat2509 <venkatms2509@gmail.com>
2017-10-17 03:50:33 -0400

    protractor e2e tests
    So far since we haven't created backend, these are not e2e per say but
    it tests basic flow.

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,39 @@
+import { Message } from '@angular/compiler/src/i18n/i18n_ast';
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
-  navigateTo() {
-    return browser.get('/');
+  navigateToCreateMeetup() {
+    return browser.get('/create-meetup');
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  getMeetUpCardTitle() {
+    return element(by.css('mat-card-title')).getText();
+  }
+
+  getButton(index) {
+    return element.all(by.tagName('button')).get(index);
+  }
+
+  getCreateMeetUpFormQuestions(index) {
+    return element.all(by.css('h3')).get(index).getText();
+  }
+
+  enterDataIntoTextArea(index, message) {
+    const firstTextBox = element.all(by.tagName('textarea')).get(index);
+    const a = message;
+    firstTextBox.clear();
+    firstTextBox.sendKeys(a);
+  }
+
+  enterDataIntoTextBox(index, message) {
+   const firstTextBox = element.all(by.tagName('input')).get(index);
+   const a = message;
+   firstTextBox.clear();
+   firstTextBox.sendKeys(a);
+  }
+
+  clickNextButton(index) {
+    element.all(by.tagName('button')).get(index).click();
+    // browser.pause();
   }
 }
