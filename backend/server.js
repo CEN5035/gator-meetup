b0f2aaf7005c22c9ebe507fec7e49ccf5c8525a2
venkat2509 <venkatms2509@gmail.com>
2017-11-07 02:28:25 -0500

    dotenv and mongo connection refactoring
    Refactored some code and used dotenv to configure enviroment variables

--- backend/server.js
+++ backend/server.js
@@ -1,46 +1,30 @@
-const express = require('express')
-const app = express()
+const express = require('express');
 const bodyParser = require('body-parser');
+const app = express();
+const path = require('path');
+const http = require('http');
+require('dotenv').load();
 
-app.use(bodyParser());
-/*************************** MongoDB Connection *********************************/
 
-var MongoClient = require('mongodb').MongoClient
-  , assert = require('assert');
+// API file for interacting with MongoDB
+const api = require('./server/routes/api');
 
-var db; 
 
-// Connection URL
-var url = 'mongodb://kartikk:GatorMeetup123@cluster0-shard-00-00-snmsy.mongodb.net:27017,cluster0-shard-00-01-snmsy.mongodb.net:27017,cluster0-shard-00-02-snmsy.mongodb.net:27017/gatormeetup?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
 
-// Use connect method to connect to the server
-MongoClient.connect(url, function(err, database) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-  db = database;
+app.use(express.static(path.join(__dirname, '../dist')));
+
+// API location
+app.use('/', api);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
+const port = process.env.PORT || 8000;
+app.set('port', port);
 
-/*************************** Express Routes *************************************/
+const server = http.createServer(app);
 
-// open server 
-var port = process.env.PORT || 8000;
-app.listen(port, function(){
-  console.log("Server listening on port 3000!");
-});
-
-// get meetups list 
-app.get('/getMeetups', function (req, res) {
-  db.collection('meetup').find().toArray(function(err, results) {
-  	console.log(results)
-  	res.send(results);
-  });
-});
-
-// post meetup 
-app.post('/postMeetup', function (req, res) {
-	db.collection('meetup').insertOne(req.body, (err, result) => {
-    if (err) return console.log(err);
-    console.log('saved to database')
-  })
-});
+server.listen(port, () => console.log(`Running on localhost:${port}`));
---
+++ backend/server/routes/api.js
@@ -0,0 +1,85 @@
+require('dotenv').config()
+const express = require('express');
+const router = express.Router();
+const MongoClient = require('mongodb').MongoClient, assert = require('assert');
+const ObjectID = require('mongodb').ObjectID
+// Connect
+var db; 
+
+// Connection URL
+console.log(process.env)
+var url = process.env.MONGO_DB_DEV_URI
+
+// Use connect method to connect to the server
+var mongo_connection = url;
+const connection = (closure) => {
+    return MongoClient.connect(mongo_connection, (err, db) => {
+        if (err) return console.log(err);
+        closure(db);
+    });
+};
+
+// Error handling
+const sendError = (err, res) => {
+    response.status = 501;
+    response.message = typeof err == 'object' ? err.message : err;
+    res.status(501).json(response);
+};
+
+// Response handling
+let response = {
+    status: 200,
+    data: [],
+    message: null
+};
+
+
+router.get('/getMeetups', (req, res) => {
+    connection((db) => {
+        db.collection('meetup')
+            .find()
+            .toArray()
+            .then((events) => {
+                response.data = events;
+                res.json(response);
+            })
+            .catch((err) => {
+                sendError(err, res);
+            });
+    });
+});
+
+router.post('/postMeetup', (req, res) => {
+    var newMeetUp = req.body;
+    console.log(req.body)
+    connection((db) => {
+        db.collection('meetup')
+            .insertOne(newMeetUp)
+            .then((events) => {
+                response.data = events;
+                res.json(response);
+            })
+            .catch((err) => {
+                sendError(err, res);
+            });
+    });
+});
+
+module.exports = router;
+
+// Without wrapped doesnt include status.
+// get meetups list 
+// router.get('/getMeetups', function (req, res) {
+//     db.collection('meetup').find().toArray(function(err, results) {
+//         console.log(results)
+//         res.send(results);
+//     });
+//   });
+  
+// post meetup 
+// router.post('/postMeetup', function (req, res) {
+//       db.collection('meetup').insertOne(req.body, (err, result) => {
+//       if (err) return console.log(err);
+//       console.log(result);
+//     })
+//   });
